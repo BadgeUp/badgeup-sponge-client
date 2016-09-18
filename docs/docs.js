00b0f686a25aad676f5f69daf4211e4449b70727
Robert Herhold <reherhold@gmail.com>
2016-09-17 20:33:12 -0400

    Data persistence docs

--- docs/docs.js
+++ docs/docs.js
@@ -13,7 +13,7 @@ renderer.code = function(code, lang) {
 
 let pages = [];
 
-const files = ['quickstart', 'events', 'awards'];
+const files = ['quickstart', 'events', 'awards', 'persistence'];
 for (let fileName of files) {
     const rawData = fs.readFileSync(path.resolve(__dirname, 'source', fileName + '.md'), {
         encoding: 'utf8'
