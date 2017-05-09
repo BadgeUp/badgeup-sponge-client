d892b181938d6c8477157c734dc75e244220943c
Robert Herhold <reherhold@gmail.com>
2017-05-09 18:44:34 -0400

    Prevent weird block modification events from being sent
    Leaves would somehow be modified by breaking adjacent leaves

--- src/main/java/io/badgeup/sponge/eventlistener/GeneralEventListener.java
+++ src/main/java/io/badgeup/sponge/eventlistener/GeneralEventListener.java
@@ -11,6 +11,8 @@ import org.json.JSONObject;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.data.Transaction;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.type.ComparatorTypes;
 import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Event;
@@ -146,9 +148,17 @@ public class GeneralEventListener extends BadgeUpEventListener {
         final UUID uuid = player.getUniqueId();
         final int increment = 1;
 
+        System.out.println(event.getTransactions().size());
         for (Transaction<BlockSnapshot> transaction : event.getTransactions()) {
             String key = keyProvider.provide(event);
-            if (event instanceof ChangeBlockEvent.Place || event instanceof ChangeBlockEvent.Modify) {
+            
+            BlockSnapshot finalBlock = transaction.getFinal();
+            // Check if the block is a "modifiable" block like a redstone-related block
+            // So we can filter out crap like leaves being changed somehow by the player
+            // Keys.DELAY is for repeaters, Keys.COMPARATOR_TYPE is for comparators
+            if (event instanceof ChangeBlockEvent.Modify && !(finalBlock.supports(Keys.POWERED) || finalBlock.supports(Keys.DELAY) || finalBlock.supports(Keys.COMPARATOR_TYPE))) {
+                continue;
+            } else if (event instanceof ChangeBlockEvent.Place || event instanceof ChangeBlockEvent.Modify) {
                 key += ":" + transaction.getFinal().getState().getType().getId();
             } else if (event instanceof ChangeBlockEvent.Break) {
                 key += ":" + transaction.getOriginal().getState().getType().getId();
